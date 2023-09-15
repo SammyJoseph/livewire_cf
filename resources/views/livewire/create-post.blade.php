ce65a773ed42f79375dbde9140ba5cb03d499114
Sammy Joseph <sam.tab.paz@gmail.com>
2023-09-15 10:46:34 -0500

    clase #8 finalizada

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -47,8 +47,10 @@
                                 @enderror
                             </div>
 
-                            <button type="submit"
-                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear Post</button>
+                            <button type="submit" wire:loading.attr="disabled"
+                                class="disabled:opacity-50 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Crear Post
+                            </button>
                         </form>
                     </div>
                 </div>
