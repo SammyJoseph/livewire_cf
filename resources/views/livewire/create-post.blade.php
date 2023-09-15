b8b202c57023d9a231db53f43d1c6261f214231d
Sammy Joseph <sam.tab.paz@gmail.com>
2023-09-14 23:37:50 -0500

    x-cloak en modales

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -5,8 +5,8 @@
         Crear Post
     </button>
 
-    <!-- Main modal -->
-    <div x-show="open"
+    <!-- Create post - Modal -->
+    <div x-show="open" x-cloak
         class="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-gray-800 bg-opacity-50 z-50">
         <form tabindex="-1" aria-hidden="true" @click.away="open = false" wire:submit="store"
             class="w-full p-4 overflow-x-hidden overflow-y-auto max-w-md bg-white shadow-lg rounded-lg">
@@ -57,7 +57,7 @@
     </div>
 
     <!-- Success toast -->
-    <div class="fixed top-32 left-1/2 transform -translate-x-1/2" x-show="openToast">
+    <div class="fixed top-32 left-1/2 transform -translate-x-1/2" x-show="openToast" x-cloak>
         <div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
             <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
